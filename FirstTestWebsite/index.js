64fd7ed870ed4e18630cc358ab25e557b1168dd1
Axl Ronquillo <ronquilloaxl1@gmail.com>
2024-12-05 19:28:24 -0500

    Added increase, decrease, and reset

--- FirstTestWebsite/index.js
+++ FirstTestWebsite/index.js
@@ -1,6 +1,16 @@
 let count = 0
 
 document.getElementById("buttonCounter").onclick = function(){
+    count -=1;
+    document.getElementById("buttonCount").innerHTML = count;
+}
+
+document.getElementById("buttonCounter2").onclick = function(){
+    count =  0;
+    document.getElementById("buttonCount").innerHTML = count;
+}
+
+document.getElementById("buttonCounter3").onclick = function(){
     count +=1;
     document.getElementById("buttonCount").innerHTML = count;
 }
